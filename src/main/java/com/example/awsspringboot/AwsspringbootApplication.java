fa43a9c4bb39a82339e7e7c03d7049f5de747016
dev22 <ysoli36512@gmail.com>
2023-06-27 15:10:49 +0900

    Initial commit

---
+++ src/main/java/com/example/awsspringboot/AwsspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsspringbootApplication.class, args);
+	}
+
+}

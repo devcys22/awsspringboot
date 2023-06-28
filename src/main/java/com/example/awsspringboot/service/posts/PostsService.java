05eeb69d0498b1366727d10628511b4e02817cf1
dev22 <ysoli36512@gmail.com>
2023-06-28 15:02:40 +0900

    Feature: 등록 Api와 테스트

---
+++ src/main/java/com/example/awsspringboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.example.awsspringboot.service.posts;
+
+import com.example.awsspringboot.domain.posts.PostsRepository;
+import com.example.awsspringboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}

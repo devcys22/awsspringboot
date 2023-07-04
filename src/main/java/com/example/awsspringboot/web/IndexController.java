382b00745f6e18245c2dd284d2dd3982ae98873f
dev22 <ysoli36512@gmail.com>
2023-07-04 21:39:01 +0900

    Feat: 게시판 수정, 삭제하기

--- src/main/java/com/example/awsspringboot/web/IndexController.java
+++ src/main/java/com/example/awsspringboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.example.awsspringboot.web;
 
 import com.example.awsspringboot.service.posts.PostsService;
+import com.example.awsspringboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +24,20 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }

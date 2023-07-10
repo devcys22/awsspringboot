951b4749b14509dc42e3f87618f751de4b7bd797
dev22 <ysoli36512@gmail.com>
2023-07-10 17:03:55 +0900

    Feat: 로그인 어노테이션으로 개선

--- src/main/java/com/example/awsspringboot/web/IndexController.java
+++ src/main/java/com/example/awsspringboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.example.awsspringboot.web;
 
+import com.example.awsspringboot.config.auth.LoginUser;
 import com.example.awsspringboot.config.auth.dto.SessionUser;
 import com.example.awsspringboot.service.posts.PostsService;
 import com.example.awsspringboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.h2.engine.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,9 +23,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
